1835462ea982daddb777b54fa8dbcdb7afd356cd
Jona Joy <jonajoy142@gmail.com>
2023-01-06 19:17:04 +0530

    Format message!

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 const app =express()
 const server = http.createServer(app)
@@ -10,26 +11,27 @@ const io = socketio(server)
 //set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
+const botName  = 'funSkoot Bot  '
 //run when client connects
 io.on('connection',socket=>{
     
     // console.log('New WS Connection..')
    
-    socket.emit('message','Welcome to FunSkoot!')
+    socket.emit('message',formatMessage(botName,'Welcome to FunSkoot!'))
 
     //BroadCast when a user connects
 
-    socket.broadcast.emit('message','A user has joined the chat')
+    socket.broadcast.emit('message',formatMessage(botName,'A user has joined the chat'))
 
     //runs when client disconnects
 
     socket.on('disconnect',()=>{
-        io.emit('message','A user left the chat')
+        io.emit('message',formatMessage(botName,'A user left the chat'))
         // console.log("disconnected");
     })
 
     socket.on('chatMessage',(msg)=>{
-        io.emit('message',msg)
+        io.emit('message',formatMessage('USER ',msg))
     })
 })
 
